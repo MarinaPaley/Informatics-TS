e3f9d2144f810de7a3ac67ac14dd2d4c1fa056af
MarinaPaley <marina_paley@mail.ru>
2021-04-06 20:16:35 +0300

    Решена задача 1-1 #3

---
+++ Work-1/Task -1.jl
@@ -0,0 +1,13 @@
+# Константы
+x = 0.29;
+y = 3.7;
+z = -1;
+
+# Вычисления
+a = 3 * x^y * log(y) + exp(x*z);
+b = abs(2*y*z) / sin(x)^2 - x^2 / 3;
+
+# ВЫвод
+println("x = $x y = $y z = $z");
+println("a = $a ");
+println("b = $b ");
